c8a3cc4bbd33c32020b812b1c997bf9b236b1606
Madiha-Bencekri <55490016+dihaben@users.noreply.github.com>
2024-06-14 16:31:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const links = {
-    "index.html": "home-link",
-    "about.html": "about-link",
-    "education.html": "education-link",
-    "experience.html": "experience-link",
-    "publications.html": "publications-link",
-    "cv.html": "cv-link",
-    "resume.html": "resume-link",
-    "social.html": "social-link",
-    "news.html": "news-link",
-    "reflections.html": "reflections-link",
-    "other.html": "other-link"
-  };
+    const links = {
+        "index.html": "home-link",
+        "about.html": "about-link",
+        "education.html": "education-link",
+        "experience.html": "experience-link",
+        "publications.html": "publications-link",
+        "cv.html": "cv-link",
+        "resume.html": "resume-link",
+        "social.html": "social-link",
+        "news.html": "news-link",
+        "reflections.html": "reflections-link",
+        "other.html": "other-link"
+    };
 
-  const currentPage = window.location.pathname.split("/").pop();
-  const activeLinkId = links[currentPage];
+    const currentPage = window.location.pathname.split("/").pop();
+    const activeLinkId = links[currentPage];
 
-  if (activeLinkId) {
-    document.getElementById(activeLinkId).classList.add("active");
-  }
+    if (activeLinkId) {
+        document.getElementById(activeLinkId).classList.add("active");
+    }
 });
